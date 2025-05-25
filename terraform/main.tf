0d52ecee018e04444379084df8c7dd5ad9d0338f
nathandxn <nathan.dixon@snyk.io>
2025-05-25 19:09:16 -0400

    feat: add subnet1 and internet gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,14 @@ resource "aws_ecr_repository" "college-scorecard-reporting-api-repository" {
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
