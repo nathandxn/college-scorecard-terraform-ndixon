e88af938e3f38b785bae748f065f9c61e28ccee6
nathandxn <nathan.dixon@snyk.io>
2025-05-25 22:38:29 -0400

    feat: attach policy to iam role

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,3 +91,9 @@ resource "aws_iam_role" "ecs_task_execution" {
     }]
   })
 }
+
+# policy grants permission to pull container images from Amazon ECR, write logs to CloudWatch, use Secrets Manager or Parameter Store
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
