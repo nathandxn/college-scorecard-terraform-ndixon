a3e33cde8ed09f4c5ebf04fe951aaaecdebe91ff
nathandxn <nathan.dixon@snyk.io>
2025-07-19 01:10:17 -0400

    feat: create subnet 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,15 +36,30 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Application = "college-scorecard-reporting-api"
+  }
+}
+
 resource "aws_subnet" "subnet1" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+
+  tags = {
+    Application = "college-scorecard-reporting-api"
+  }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main_vpc.id
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 
   tags = {
     Application = "college-scorecard-reporting-api"
