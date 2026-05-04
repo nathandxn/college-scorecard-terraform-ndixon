53c96c257166760784c958d8cd622c4924d850fb
nathandxn <nathandxn@gmail.com>
2026-05-04 15:19:26 -0400

    feat: add api_url to outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "alb_dns_name" {
   description = "Public DNS name for the reporting API load balancer"
   value       = aws_lb.reporting_api_lb.dns_name
 }
+
+output "api_url" {
+  description = "Public HTTPS URL for the reporting API"
+  value       = "https://${var.domain_name}"
+}
